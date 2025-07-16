5d73d904d3eb809e34e6d973027c46b5d13703d2
LeonardoRazzai <xcoderx00@gmail.com>
2025-07-16 18:21:35 +0200

    gain read from fits files

--- TempAnalyzer.py
+++ TempAnalyzer.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.optimize import curve_fit
 from astropy.io import fits
 
-MEDIUM_SIZE = 13
+MEDIUM_SIZE = 14
 BIGGER_SIZE = 15
 
 plt.rc('font', size=MEDIUM_SIZE)          # controls default text sizes
@@ -24,28 +24,28 @@ title_font = {
   'weight' : 'bold'
 }
 
-# Define constants
-# M = 0.175 # magnification (nominally 0.25)
-# err_rel_M = 0.02
+M_pix = 53 # pix/mm calibration 2 with mot (23-06-2025)
+err_rel_M = 1/M_pix
+PIXEL_SIZE = 1/M_pix
 
-# M = 0.4 # magnification from p/q
-M = 0.33 # magnification from last calibration with mot (23-06-2025)
-err_rel_M = 0.05 # relative error in magnification
-PIXEL_SIZE = 0.0069 / M # mm # pixel size in MOT plane
 M_Rb87 = 86.909 * 1.660539e-27 # Rb87 mass in kg
 KB = 1.38064852e-23 # Boltzmann constant in J/K
 
 # path to image folder
 PATH_TO_IMG = './img'
+Gamma = 2 * np.pi * 6.065e6 # Hz
 
 class Image:
     def __init__(self, image):
         self.image = image
         with fits.open(f'{PATH_TO_IMG}/{image}') as hdul:
             self.im = hdul[0].data
+            hdr = hdul[0].header
+            self.Gain = hdr['GAIN'] # gain in dB
             
         self.row_sum = self.im.sum(axis=0)
         self.col_sum = self.im.sum(axis=1)
+        self.tot_counts = self.im.sum()
         
         self.mu_x = 0.0
         self.mu_y = 0.0 
@@ -54,12 +54,20 @@ class Image:
     
     def select_roi(self, y1, y2, x1, x2):
         self.im = self.im[y1:y2, x1:x2]
+        self.row_sum = self.im.sum(axis=0)
+        self.col_sum = self.im.sum(axis=1)
+        self.tot_counts = self.im.sum()
     
     def show_img(self):
         plt.imshow(self.im)
+        plt.colorbar()
         plt.show()
     
     def plot_axis(self, axis='x'):
+        self.row_sum = self.im.sum(axis=0)
+        self.col_sum = self.im.sum(axis=1)
+        self.tot_counts = self.im.sum()
+        
         if axis == 'x':
             xdata = np.arange(len(self.row_sum))
             ydata = np.array(self.row_sum)
@@ -79,6 +87,10 @@ class Image:
         plt.close()
     
     def fit_gaussian(self, axis='x', plot=False):
+        self.row_sum = self.im.sum(axis=0)
+        self.col_sum = self.im.sum(axis=1)
+        self.tot_counts = self.im.sum()
+        
         if axis == 'x':
             xdata = np.arange(len(self.row_sum))
             ydata = np.array(self.row_sum)
@@ -95,15 +107,20 @@ class Image:
             if axis == 'x':
                 self.mu_x = popt[1]
                 self.sigma_x = abs(popt[2])
+                self.int_x = popt[0] * np.sqrt(2 * np.pi) * self.sigma_x
+                self.cx = popt[3] * len(xdata) # baseline counts
+                
             elif axis == 'y':
                 self.mu_y = popt[1]
                 self.sigma_y = abs(popt[2])
+                self.int_y = popt[0] * np.sqrt(2 * np.pi) * self.sigma_y
+                self.cy = popt[3] * len(xdata) # baseline counts
                          
             if plot:
                 plt.plot(xdata, ydata)
                 plt.plot(xdata, gaussian(xdata, *popt), '--', color='red', label='Gaussian: ' + f'sigma={popt[2]:.0f}')
                 plt.legend()
-                plt.title(f'Fit of 2 Gaussians along {axis} axis')
+                plt.title(f'Gaussian Fit along {axis} axis')
                 plt.xlabel(f'{axis} (pixels)')
                 plt.ylabel('Intensity')
                 plt.grid()
@@ -117,13 +134,51 @@ class Image:
 
         return popt
     
+    def Get_Number_Of_Atoms(self, axis='x'):
+        """
+        Calculate the number of atoms in the image from Integral of the Gaussian.
+        
+        NB: Assume 16 bit format, 12 bit depth
+        """
+        
+        G_dB = self.Gain
+        G = 10**(G_dB / 20) # convert dB to linear scale
+        eta = 0.28 # quantum efficiency of the camera at 780 nm (Datasheet)
+        eADU_0dB = 110.7 # 0.35 at 50 dB, 12 bit
+        eADU_G = eADU_0dB / G # e/ADU at gain G
+        bit_depth = 12
+        bit_format = 16
+        
+        R_lens = 1.42 # aperture radius in cm
+        dist_mot = 18 # cm
+        fractional_sigma = 0.25 * (R_lens/dist_mot)**2
+        t_pulse = 300e-6 # s        
+
+        Delta = 2.2 * Gamma
+        N_ph_per_atom = SC_Rate(Delta) * t_pulse
+        N_el_per_atom = N_ph_per_atom * fractional_sigma * eta
+        N_counts_per_atom = N_el_per_atom / eADU_G * 2**(bit_format - bit_depth)
+        
+        if axis == 'x':
+            N_counts_MOT = self.int_x
+        elif axis == 'y':
+            N_counts_MOT = self.int_y
+        else:
+            raise ValueError("Invalid axis")
+        
+        N_atoms = N_counts_MOT / N_counts_per_atom
+        print(f'Number of Atoms = {N_atoms:.2e}')
+        print(f'Integral of Gaussian = {N_counts_MOT:.2e}, N_tot by diff. = {self.tot_counts - self.cx:.2e}')
+        
+        return N_atoms, 0.1*N_atoms
+    
 
 def gaussian(x, A, mu, sigma, C):
     return A * np.exp(-0.5 * ((x - mu) / sigma)**2) + C
 
-def get_temperature(im1: Image, im2: Image, t1: float, t2: float):
+def Get_temperature(im1: Image, im2: Image, t1: float, t2: float):
     """
-    Calculate the temperature from the variance and time of flight.
+    Calculate the temperature from the variance along x and time of flight.
     """
     var1 = im1.sigma_x**2 * PIXEL_SIZE**2
     var2 = im2.sigma_x**2 * PIXEL_SIZE**2
@@ -137,3 +192,78 @@ def get_temperature(im1: Image, im2: Image, t1: float, t2: float):
     dT = dv_2 * M_Rb87 /  KB * 1e6
     
     return T, dT
+
+def SC_Rate(Delta):
+    s0 = 34 # saturation parameter P = 65 mW
+    return Gamma / 2 * s0 / (1 + s0 + 4 * (Delta / Gamma)**2)
+
+def Analysis_Num_and_T(dataset, t1, t2):
+    
+    base_name = dataset['base_name']
+    x_label = dataset['x_label']
+    unit = dataset['unit']
+    iterator = dataset['iterator']
+
+    T_list = []
+    err_T_list = []
+
+    N_atoms_list = []
+    err_N_atoms_list = []
+    
+    for val in iterator:
+        print('----------------------------------------------------------')
+        im1 = Image(f'{base_name}={val}{unit}_tof={t1:.1f}ms.fits.gz')
+        im1.select_roi(0, 400, 0, 600)
+        im1.fit_gaussian('x', plot=False)
+        
+        im2 = Image(f'{base_name}={val}{unit}_tof={t2:.1f}ms.fits.gz')
+        im2.select_roi(0, 400, 0, 600)
+        im2.fit_gaussian('x', plot=False)
+        
+        N_atoms, err_N_atoms = im2.Get_Number_Of_Atoms(axis='x')
+        
+        N_atoms_list.append(N_atoms)
+        err_N_atoms_list.append(err_N_atoms)
+        
+        T, dT = Get_temperature(im1, im2, t1, t2)
+        
+        T_list.append(T)    
+        err_T_list.append(dT)
+        
+        print(f'{x_label}: {val}, Temp: ({T:.2f} +- {dT:.2f}) uK\n')
+        
+    return T_list, err_T_list, N_atoms_list, err_N_atoms_list
+
+def Analysis_and_Plotting(dataset):
+    
+    base_name = dataset['base_name']
+    x_label = dataset['x_label']
+    unit = dataset['unit']
+    iterator = dataset['iterator']
+    
+    T_list, err_T_list, N_atoms_list, err_N_atoms_list = Analysis_Num_and_T(dataset, t1=2.0, t2=20.0)
+        
+    # plotting
+    plt.figure(figsize=(8, 5))
+    plt.errorbar(iterator, T_list, err_T_list, fmt='o', alpha=0.9, color='blue', ecolor='royalblue', capsize=5, label='Data')
+    plt.xlabel(f'{x_label} ({unit})', fontdict=base_font)
+    plt.ylabel('Temp (uK)', fontdict=base_font)
+    plt.title(f'Temperature vs {x_label}', fontdict=title_font)
+    plt.grid(linestyle='--', alpha=0.5)
+    plt.tight_layout()
+    plt.savefig(f'Temp_vs_{base_name}.png')
+    plt.clf()
+    plt.close()
+    
+    N_atoms = np.array(N_atoms_list) * 1e-8  # convert to 10^8
+    err_N_atoms = np.array(err_N_atoms_list) * 1e-8  # assuming 10% error
+    plt.figure(figsize=(8, 5))
+    plt.errorbar(iterator, N_atoms, err_N_atoms, fmt='o', alpha=0.9, color='blue', ecolor='royalblue', capsize=5, label='Data')
+    plt.xlabel(f'{x_label} ({unit})', fontdict=base_font)
+    plt.ylabel(r'Num Atoms ($10^8$)', fontdict=base_font)
+    plt.title(f'Num of Atoms vs {x_label}', fontdict=title_font)
+    plt.grid(linestyle='--', alpha=0.5)
+    plt.tight_layout()
+    plt.savefig(f'Num_atoms_vs_{base_name}.png')
+    plt.clf()
+    plt.close()  
