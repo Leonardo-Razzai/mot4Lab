d29546ebbdebc26d25a952327b1ff8254c85f9ee
LeonardoRazzai <leonardo.razzai2@unibo.it>
2025-07-23 18:18:04 +0200

    add data and script for acqusition

--- mot_and_photo.py
+++ mot_and_photo.py
@@ -1,14 +1,15 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
-from MOTAcqLib import acquire_img, setup_camera
+from MOTAcqLib import *
 
 CAM_GAIN = 0 # dB
 CAM_EXP_TIME = 5000 # us
-PROBE_TIME = 300 # us
+PROBE_TIME = 180 # us
 
-fits_fname = 'mot_displeacement_shimz=-500mA'
-mot_fname = 'mot_displeacement.mot'
+fits_fname = F'CCD_t_probe={PROBE_TIME}'
+mot_base_name = 'bkg'
 
 setup_camera(gain=CAM_GAIN, exp_time=CAM_EXP_TIME)
-
-acquire_img(mot_fname, fits_fname, t_probe=PROBE_TIME)
+mot_fname = write_mot_file(mot_base_name, t_probe=PROBE_TIME)
+acquire_img(mot_fname=mot_fname, fits_fname=fits_fname, t_probe=PROBE_TIME)
+show_img(img_folder + fits_fname)
