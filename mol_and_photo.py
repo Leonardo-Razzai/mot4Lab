d6329ecd7426ea2baba732a980173872f41fa66f
LeonardoRazzai <leonardo.razzai2@unibo.it>
2025-12-15 17:48:41 +0100

    add roi selection

--- mol_and_photo.py
+++ mol_and_photo.py
@@ -1,15 +1,19 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 from MOTAcqLib import *
+from TempAnalyzer import *
 
 CAM_GAIN = 0 # dB
-CAM_EXP_TIME = 5000 # us
-PROBE_TIME = 400 # us
+CAM_EXP_TIME = 1000 # us
+PROBE_TIME = 500 # us
+TOF=35 # ms
 
-fits_fname = F'MOTtest_t_probe={PROBE_TIME}'
+fits_fname = F'odt=500mW'
 mot_base_name = 'molasses_and_tof'
 
 setup_camera(gain=CAM_GAIN, exp_time=CAM_EXP_TIME)
-mot_fname = write_mot_file(mot_base_name, t_probe=PROBE_TIME, tof_val=1)
+mot_fname = write_mot_file(mot_base_name, t_probe=PROBE_TIME, tof_val=TOF)
 acquire_img(mot_fname=mot_fname, fits_fname=fits_fname, t_probe=PROBE_TIME)
-show_img(img_folder + fits_fname)
+img = Image(img_folder + fits_fname + '.fits.gz')
+img.select_roi(0, 450, 100, 600)
+img.show_img()
