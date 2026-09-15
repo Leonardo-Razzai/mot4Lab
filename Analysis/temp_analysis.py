3f884c84deb7189c5adcfd051fd7a51185751c34
LeonardoRazzai <leonardo.razzai2@unibo.it>
2026-09-15 16:48:59 +0200

    adapt to new scripts and manta

--- Analysis/temp_analysis.py
+++ Analysis/temp_analysis.py
@@ -7,20 +7,20 @@ sys.path.append(parent_dir)
 
 from Modules.TempAnalyzer import *
 
-date = '2025-12-05'
+date = '2026-09-15'
 data_folder = '../raw_data/'+ f'{date}/img/'
 
 def f_base_name(t):
-    return f'T_meas_test_tof={t:.1f}ms.fits.gz'
+    return f'optimal_loading_tof={t:.1f}ms.fits.gz'
 
-t1 = 2. # ms
-t2 = 20. # ms
+t1 = 5. # ms
+t2 = 30. # ms
 
 img1 = Image(data_folder + f_base_name(t1))
 img2 = Image(data_folder + f_base_name(t2))
 
-img1.select_roi(0, 300, 0, 500)
-img2.select_roi(0, 300, 0, 500)
+img1.select_roi(0, 1100, 500, 1500)
+img2.select_roi(0, 1100, 500, 1500)
 
 img1.fit_gaussian(plot=True)
 img2.fit_gaussian(plot=True)
