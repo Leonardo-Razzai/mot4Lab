9fa6355699561ef7cd0edad6554d3a60f219d734
LeonardoRazzai <leonardo.razzai2@unibo.it>
2026-09-15 16:52:43 +0200

    New routine to load standing wave

---
+++ Routines/ODT_lifetime_stading_wave/2026-09-08/load_odt_standing_wave.py
@@ -0,0 +1,66 @@
+import time
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+grandparent_dir = os.path.dirname(os.path.dirname(current_dir))
+grand_grandparent_dir  = os.path.dirname(os.path.dirname(os.path.dirname(current_dir)))
+grand_grand_grandparent_dir  = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(current_dir))))
+sys.path.append(grandparent_dir)
+sys.path.append(grand_grandparent_dir)
+sys.path.append(grand_grand_grandparent_dir)
+
+from Modules.MOTAcqLib import *
+from Modules.TempAnalyzer import *
+
+
+MOT_BASE_NAME = 'load_odt_standing_wave'
+FITS_BASE_NAME = MOT_BASE_NAME + 't_odt'
+REPETITIONS = 5
+
+# param values are set in the for loop
+RT_PARAMS= {
+    '<todt>':    0,
+    '<ODTamp>' : 0,
+    '<ODTBlackAmp>' :0,
+}
+
+# cam params
+CAM_GAIN = 6 #dB
+CAM_EXP_TIME = 1000 #us
+
+# ODT params
+ODTAmp = 420
+ODTBlackamp = 880
+
+print("\n"+ "-"*30)
+
+# time of flight with odt off, before photo
+t_odt_values = range(30, 40, 3)
+
+for t_odt in t_odt_values:
+	t_odt=t_odt-0.1
+	RT_PARAMS['<todt>'] = t_odt
+	print(f't_odt= {t_odt}')
+	
+	for i in range(1, REPETITIONS + 1):
+		fits_fname = FITS_BASE_NAME + (f'={t_odt}_{i:02d}')
+
+		# bkg acquisition
+		RT_PARAMS['<ODTamp>'] = 0
+		RT_PARAMS['<ODTBlackamp>'] = 0
+		fits_fname = 'odt_off_'+ fits_fname
+		setup_camera(gain=CAM_GAIN, exp_time=CAM_EXP_TIME, n=1, manta=True)
+		print(' --- Start bkg -- '+fits_fname)
+		write_and_acquire_mot(MOT_BASE_NAME, fits_fname, params=RT_PARAMS, n=1, manta=True)
+		time.sleep(3)
+		
+		# ODT On acquisition
+		fits_fname = FITS_BASE_NAME + (f'={t_odt}_{i:02d}')
+		RT_PARAMS['<ODTamp>'] = ODTAmp
+		RT_PARAMS['<ODTBlackamp>'] = ODTBlackamp
+		setup_camera(gain=CAM_GAIN, exp_time=CAM_EXP_TIME, n=1, manta=True)
+		print(' --- Start exp -- '+fits_fname)
+		write_and_acquire_mot(MOT_BASE_NAME, fits_fname, params=RT_PARAMS, n=1, manta=True)
+		time.sleep(3)
+
